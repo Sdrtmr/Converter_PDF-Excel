1fc9a29829a83c2fce1cdda207da3744147dd216
deni1gugu <150820654+Sdrtmr@users.noreply.github.com>
2023-11-28 13:49:03 +0300

    Changes in the structure of the code due to its breakage
    Commiting changes in code

---
+++ File functional code.py
@@ -0,0 +1,25 @@
+# installing tabula
+
+ # pip install tabula-py
+
+ # import pandas and convert
+
+import tabula 
+import pandas as pd
+
+pdf_in = "D:/Folder/File.pdf"
+
+PDF = tabula.read_pdf(pdf_in, pages = 'all') 
+
+# view result
+
+print ('\nTables from PDF file\n'+str(PDF))
+
+pdf_out_xlsx = "D:\Temp\From_PDF.xlsx"
+
+# converting to Excel 
+
+PDF = pd.DataFrame(PDF)
+PDF.to_excel(pdf_out_xlsx,index=False) 
+
+print("Done") 
